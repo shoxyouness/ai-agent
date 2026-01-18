b9d8a7b9d490016c4b50d24816a4bb5ea14d765a
momha003 <mohamed.mhadhbi003@stud.fh-dortmund.de>
2026-01-18 16:50:05 +0100

    feat: Initialize Next.js frontend project with standalone output and essential dependencies.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    config.externals.push({
+      "node:path": "commonjs path",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
